0d6ba793c6477387a7dbf0ce7ab75812a615feb6
anoldkiriago <kiriagoarnold@gmail.com>
2023-07-14 14:01:42 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+int Lowercase = 'a';
+int Uppercase = 'A';
+while (Lowercase <= 'z')
+{
+putchar(Lowercase);
+Lowercase += 1;
+}
+while (Uppercase <= 'z')
+{
+putchar(Uppercase);
+Uppercase += 1;
+}
+{
+putchar('\n');
+return (0);
+}
