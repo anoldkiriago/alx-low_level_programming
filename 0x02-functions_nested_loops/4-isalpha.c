0eb465fc9b9ac49d0725682f32de46e4bcaddef6
anoldkiriago <kiriagoarnold@gmail.com>
2023-07-18 05:06:53 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,17 +2,20 @@
 
 /**
  * _isalpha - Entry point
- * @c: The character to be checked
+ * @c: The character being searched
  *
- * Return: 1 for alphabetic  character or 0 for anything else
+ * Return: 1 for letters. 0 for the rest
  */
 
 int _isalpha(int c)
 {
-
-if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
-{
-return (1);
-}
-return (0);
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
 }
