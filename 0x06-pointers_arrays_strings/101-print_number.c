f4c23a8bcaa3ae580b421fd25f5634c11d9a2bfb
anoldkiriago <kiriagoarnold@gmail.com>
2023-07-27 02:30:32 -0700

    101-print_numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,7 +12,7 @@ void print_number(int n)
 	n1 = n;
 	if (n < 0)
 	{
-		putchar('_');
+		putchar('-');
 		n1 = -n;
 	}
 	if (n1 / 10 != 0)
---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,5 @@
+#include "main.h"
+/**
+ * infinite_add - Entry point
+ * @n1:
+ * @n2
